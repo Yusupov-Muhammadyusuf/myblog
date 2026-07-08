b58f729eafd4d99e2e467cc5e2bb8b8fcb622b8f
YM.devProg <muhammadyusuf.yusupov201@gmail.com>
2026-07-08 20:38:50 +0500

    feat: add core app modules

---
+++ blog/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseForbidden
+from .models import Post
+from .forms import PostForm
+
+def blog_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    categories = Post.objects.values_list('category', flat=True).distinct()
+    return render(request, 'post.html', {'posts': posts, 'categories': categories})
+
+def create_post(request):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = PostForm()
+        
+    return render(request, 'write.html', {'form': form})
+
+def edit_post(request, pk):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden()
+        
+    post = get_object_or_404(Post, pk=pk)
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            next_url = request.POST.get('next_url')
+            return redirect(next_url if next_url else 'blog_list')
+    else:
+        form = PostForm(instance=post)
+        next_url = request.META.get('HTTP_REFERER')
+
+    return render(request, 'edit.html', {
+        'form': form, 
+        'post': post,
+        'next_url': next_url
+    })
+
+def delete_post(request, pk):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden()
+        
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+    return redirect('blog_list')
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
