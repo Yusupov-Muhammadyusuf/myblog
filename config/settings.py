07877f2c983dadf867e977034bd89aa3fe5c6639
YM.devProg <muhammadyusuf.yusupov201@gmail.com>
2026-07-11 07:59:27 +0500

    fixing database connection
    Updated DEBUG setting to True for development.

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9-!#=g*o&l97j@y#$s9nflk1^ge0fekuiore-&znnd0i7oh-do"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
