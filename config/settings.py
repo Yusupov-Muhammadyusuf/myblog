9ab1122b407b8c6fb2ba04d8a8149236cf2fa9fb
Yusupov-Muhammadyusuf <muhammadyusuf.yusupov201@gmail.com>
2026-07-21 21:22:34 +0500

    fix: deploy issue

--- config/settings.py
+++ config/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9-!#=g*o&l97j@y#$s9nflk1^ge0fekuiore-&znnd0i7oh-do"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['yusupov.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
