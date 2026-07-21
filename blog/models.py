6ff5952918682084aa2d88a46cb863a7ab294b0c
Yusupov-Muhammadyusuf <muhammadyusuf.yusupov201@gmail.com>
2026-07-21 21:04:15 +0500

    feat: add ckeditor for post content

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
 class Post(models.Model):
     category = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = CKEditor5Field('Content', config_name='default')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
